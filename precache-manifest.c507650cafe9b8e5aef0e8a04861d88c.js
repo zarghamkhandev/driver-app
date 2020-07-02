affc82347d3aaea9385717006cf3783cf9065013
unknown <zarghamkhan980@gmail.com>
2020-07-02 09:34:13 +0200

    Updates

---
+++ precache-manifest.c507650cafe9b8e5aef0e8a04861d88c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd3f3f4bb62bd5bc68a8c64feb8c542",
+    "url": "/driver-app/index.html"
+  },
+  {
+    "revision": "1d34f1e40b79ea807285",
+    "url": "/driver-app/static/css/main.59294f42.chunk.css"
+  },
+  {
+    "revision": "c188725deb17f1a1e3e7",
+    "url": "/driver-app/static/js/2.25e13076.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/driver-app/static/js/2.25e13076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d34f1e40b79ea807285",
+    "url": "/driver-app/static/js/main.80ccfa4f.chunk.js"
+  },
+  {
+    "revision": "0dcdc760ca9c3756778a",
+    "url": "/driver-app/static/js/runtime-main.86cc5742.js"
+  }
+]);
---
+++ static/js/main.80ccfa4f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdriver-app"]=this["webpackJsonpdriver-app"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(2),p=n.n(c);n(8);var i=function(){return r.a.createElement("div",{className:"App"},r.a.createElement("h1",null,"Hi i am your app"))};p.a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(i,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.80ccfa4f.chunk.js.map
