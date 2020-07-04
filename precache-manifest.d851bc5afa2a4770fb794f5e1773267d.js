7ec81f4c80dc2bf208c22d58b2a714bf8bdc24f7
zarghamkhandev <zarghamkhan980@gmail.com>
2020-07-04 16:58:22 +0200

    Updates

---
+++ precache-manifest.d851bc5afa2a4770fb794f5e1773267d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbb375c9b1d0a5a9033886d4ebe2ceb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a296abdc791c4abce0be",
+    "url": "./static/css/main.534c1126.chunk.css"
+  },
+  {
+    "revision": "ba86dbdd981257ff0dae",
+    "url": "./static/js/2.f2299fc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f2299fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a296abdc791c4abce0be",
+    "url": "./static/js/main.e30b5743.chunk.js"
+  },
+  {
+    "revision": "273914ecbd984d593fa8",
+    "url": "./static/js/runtime-main.4afdeb6a.js"
+  },
+  {
+    "revision": "f19866b6be862df56b4e35e784d56093",
+    "url": "./static/media/Mont-Semibold.f19866b6.ttf"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "./static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "d815b0a29adf3450c55f56e2fb813be4",
+    "url": "./static/media/Montserrat-Medium.d815b0a2.otf"
+  },
+  {
+    "revision": "92db9a0772b3732e6d686fec3711af42",
+    "url": "./static/media/Montserrat-Regular.92db9a07.otf"
+  },
+  {
+    "revision": "53d7447de8ee822bb4cec5f24e809015",
+    "url": "./static/media/ScrollButton.53d7447d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/driver-app/precache-manifest.714126a5096dfec435909bb1c428fdf4.js"
+  "./precache-manifest.d851bc5afa2a4770fb794f5e1773267d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/driver-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
