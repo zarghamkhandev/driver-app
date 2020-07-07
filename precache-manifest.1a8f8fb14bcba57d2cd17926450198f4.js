efdb250901330f0f5f3ccebd39ee14d92e5b5a10
zarghamkhandev <zarghamkhan980@gmail.com>
2020-07-07 19:29:37 +0200

    Updates

---
+++ precache-manifest.1a8f8fb14bcba57d2cd17926450198f4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9917e6caa929b8b0af1ebdccbcc18f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "66f4824735a769c0bac1",
+    "url": "./static/css/main.3564e70d.chunk.css"
+  },
+  {
+    "revision": "a1dc7f6a1f561d05161b",
+    "url": "./static/js/2.d385ccea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d385ccea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f4824735a769c0bac1",
+    "url": "./static/js/main.a7d6d125.chunk.js"
+  },
+  {
+    "revision": "273914ecbd984d593fa8",
+    "url": "./static/js/runtime-main.4afdeb6a.js"
+  },
+  {
+    "revision": "f19866b6be862df56b4e35e784d56093",
+    "url": "./static/media/Mont-Semibold.f19866b6.ttf"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "./static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "d815b0a29adf3450c55f56e2fb813be4",
+    "url": "./static/media/Montserrat-Medium.d815b0a2.otf"
+  },
+  {
+    "revision": "92db9a0772b3732e6d686fec3711af42",
+    "url": "./static/media/Montserrat-Regular.92db9a07.otf"
+  },
+  {
+    "revision": "bb3740d350b0186ce32b5678972bf061",
+    "url": "./static/media/Montserrat-SemiBold.bb3740d3.otf"
+  },
+  {
+    "revision": "53d7447de8ee822bb4cec5f24e809015",
+    "url": "./static/media/ScrollButton.53d7447d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d766f3b411b33605c54dc900ea1cc50a.js"
+  "./precache-manifest.1a8f8fb14bcba57d2cd17926450198f4.js"
 );
 
 self.addEventListener('message', (event) => {
